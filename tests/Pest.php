961b9351aefd984fea7d5420efb6f8d8ab3b7530
Sebastian Fix <sebastian.fix@codebar.ch>
2023-03-01 23:02:19 +0100

    WIP

---
+++ tests/Pest.php
@@ -0,0 +1,5 @@
+<?php
+
+use CodebarAg\FeaturePolicy\Tests\TestCase;
+
+uses(TestCase::class)->in(__DIR__);
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,12 +5,19 @@ namespace CodebarAg\FeaturePolicy\Tests;
 use CodebarAg\FeaturePolicy\FeaturePolicyServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
-abstract class TestCase extends Orchestra
+class TestCase extends Orchestra
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+    }
+
     protected function getPackageProviders($app)
     {
         return [
             FeaturePolicyServiceProvider::class,
         ];
     }
+
 }
