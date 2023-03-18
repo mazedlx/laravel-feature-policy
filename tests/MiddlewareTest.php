bb0f0270f6f2088fa731238b5b09e86a1649c7d5
Tonko Mulder <tonko@hihaho.com>
2023-03-18 21:00:09 +0100

    remove duplicate tests

--- tests/MiddlewareTest.php
+++ tests/MiddlewareTest.php
@@ -2,23 +2,6 @@
 
 namespace Mazedlx\FeaturePolicy\Tests;
 
-use Symfony\Component\HttpFoundation\HeaderBag;
-
 class MiddlewareTest extends TestCase
 {
-    /** @test */
-    public function it_sets_the_default_feature_policy_headers()
-    {
-        $headers = $this->getResponseHeaders();
-
-        $this->assertStringContainsString('geolocation=self', $headers->get('Permissions-Policy'));
-    }
-
-    protected function getResponseHeaders(string $url = 'test-route'): HeaderBag
-    {
-        return $this
-            ->get($url)
-            ->assertSuccessful()
-            ->headers;
-    }
 }
